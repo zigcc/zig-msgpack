e0686a7554b59a13b48e175fd9477a69c38e8bd8
jinzhongjia <mail@nvimer.org>
2024-01-12 22:04:02 +0800

    add str unit-test

--- src/uint-test.zig
+++ src/uint-test.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const msgpack = @import("msgpack.zig");
+const allocator = std.testing.allocator;
 const expect = std.testing.expect;
 
 const Buffer = msgpack.Buffer;
@@ -179,4 +180,53 @@ test "f64 write and read" {
     const val = try p.read_float();
 
     try expect(val == test_val);
+}
+
+test "fix_str write and read" {
+    var arr: [0xffff]u8 = std.mem.zeroes([0xffff]u8);
+    var buf = Buffer{ .arr = &arr };
+    var p = packType{ .context = &buf };
+
+    const test_val = "Hello, world!";
+    try p.write_str(test_val);
+    const val = try p.read_str(allocator);
+    defer allocator.free(val);
+    try expect(std.mem.eql(u8, test_val, val));
+}
+
+test "u8 str write and read" {
+    var arr: [0xffff]u8 = std.mem.zeroes([0xffff]u8);
+    var buf = Buffer{ .arr = &arr };
+    var p = packType{ .context = &buf };
+
+    const test_val = "This is a string that is more than 32 bytes long.";
+    try p.write_str(test_val);
+    const val = try p.read_str(allocator);
+    defer allocator.free(val);
+    try expect(std.mem.eql(u8, test_val, val));
+}
+
+test "u16 str write and read" {
+    var arr: [0xffff]u8 = std.mem.zeroes([0xffff]u8);
+    var buf = Buffer{ .arr = &arr };
+    var p = packType{ .context = &buf };
+
+    const test_val = "When the zig test tool is building a test runner, only resolved test declarations are included in the build. Initially, only the given Zig source file's top-level declarations are resolved. Unless nested containers are referenced from a top-level test declaration, nested container tests will not be resolved.";
+    try p.write_str(test_val);
+    const val = try p.read_str(allocator);
+    defer allocator.free(val);
+    try expect(std.mem.eql(u8, test_val, val));
+}
+
+test "u32 str write and read" {
+    var arr: [0xffff_f]u8 = std.mem.zeroes([0xffff_f]u8);
+    var buf = Buffer{ .arr = &arr };
+    const p = packType{ .context = &buf };
+
+    const default_str = "0123456789564562";
+    const test_val = @as([16:0]u8, default_str.*) ** (0xfff * 2);
+    try p.write_str(&test_val);
+    const val = try p.read_str(allocator);
+    defer allocator.free(val);
+    try expect(std.mem.eql(u8, &test_val, val));
 }
