4425d9e9154575c45b4905991111c734e9bab5a1
jinzhongjia <mail@nvimer.org>
2024-02-10 17:50:32 +0800

    add basic rpc part

--- src/msgpack_rpc.zig
+++ src/msgpack_rpc.zig
@@ -2,8 +2,10 @@
 //! https://github.com/msgpack-rpc/msgpack-rpc
 
 const std = @import("std");
+const builtin = @import("builtin");
 const msgpack = @import("msgpack");
 const Allocator = std.mem.Allocator;
+const net = std.net;
 
 /// the types of msssage pack rpc
 const MessageType = enum(u2) {
@@ -15,4 +17,51 @@ const MessageType = enum(u2) {
 const Header = struct {
     t: MessageType,
     id: ?u32,
+};
+
+pub const TcpServer = struct {
+    allocator: Allocator,
+    id: u32 = 0,
+    server: net.StreamServer,
+
+    /// init TcpServer
+    pub fn init(allocator: Allocator) TcpServer {
+        const server = net.StreamServer.init(net.StreamServer.Options{
+            .reuse_port = true,
+        });
+
+        return TcpServer{
+            .allocator = allocator,
+            .server = server,
+        };
+    }
+
+    /// deinit
+    pub fn deinit(self: *TcpServer) void {
+        self.server.deinit();
+    }
+
+    pub fn listen(self: *TcpServer, address: []const u8, port: u16) !void {
+        const loopback = try net.Ip4Address.parse(address, port);
+        const host = net.Address{ .in = loopback };
+
+        try self.server.listen(host);
+    }
+};
+
+pub const UnixSocket = struct {
+    allocator: Allocator,
+    id: u32 = 0,
+    stream: std.net.Stream,
+
+    /// init to create unix socket
+    /// this is only useful for unix
+    pub fn init(allocator: Allocator, path: []const u8) !UnixSocket {
+        const stream = try std.net.connectUnixSocket(path);
+
+        return UnixSocket{
+            .allocator = allocator,
+            .stream = stream,
+        };
+    }
 };
