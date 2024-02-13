f75ec241326709e40dbeb49ec9afa1bdb82343e4
jinzhongjia <mail@nvimer.org>
2024-02-13 22:53:07 +0800

    update rpc support

--- src/msgpack_rpc.zig
+++ src/msgpack_rpc.zig
@@ -4,11 +4,19 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const msgpack = @import("msgpack.zig");
+const comptimePrint = std.fmt.comptimePrint;
 const wrapStr = msgpack.wrapStr;
 const Allocator = std.mem.Allocator;
 const net = std.net;
 
-const streamPack = msgpack.Pack(net.Stream, net.Stream, net.Stream.WriteError, net.Stream.ReadError, net.Stream.write, net.Stream.read);
+const streamPack = msgpack.Pack(
+    net.Stream,
+    net.Stream,
+    net.Stream.WriteError,
+    net.Stream.ReadError,
+    net.Stream.write,
+    net.Stream.read,
+);
 
 /// the types of msssage pack rpc
 const MessageType = enum(u2) {
@@ -22,87 +30,189 @@ const Header = struct {
     id: ?u32,
 };
 
-pub const TcpServer = struct {
-    allocator: Allocator,
-    id: u32 = 0,
-    server: net.StreamServer,
+pub const TcpClient = Peer(std.net.Stream);
 
-    /// init TcpServer
-    pub fn init(allocator: Allocator) TcpServer {
-        const server = net.StreamServer.init(net.StreamServer.Options{
-            .reuse_port = true,
-        });
-
-        return TcpServer{
-            .allocator = allocator,
-            .server = server,
-        };
-    }
-
-    /// deinit
-    pub fn deinit(self: *TcpServer) void {
-        self.server.deinit();
-    }
-
-    pub fn listen(self: *TcpServer, address: []const u8, port: u16) !void {
-        const loopback = try net.Ip4Address.parse(address, port);
-        const host = net.Address{ .in = loopback };
-
-        try self.server.listen(host);
-    }
+/// ResponseMessage for Job
+const RequestMessage = struct {
+    msgid: u32,
+    method: []const u8,
+    /// this is a buffer which store param data
+    params: []const u8,
 };
 
-pub const TcpClient = struct {
-    allocator: Allocator,
-    id: u32 = 1,
-    stream: net.Stream,
-    pack: streamPack,
-
-    pub fn init(allocator: Allocator, address: []const u8, port: u16) !TcpClient {
-        const peer = try net.Address.parseIp4(address, port);
-        const stream = try net.tcpConnectToAddress(peer);
-        return TcpClient{
-            .allocator = allocator,
-            .stream = stream,
-            .pack = streamPack.init(stream, stream),
-        };
-    }
-
-    pub fn deinit(client: *TcpClient) void {
-        client.stream.close();
-    }
-
-    pub fn call(client: *TcpClient, method: []const u8, params: anytype) !void {
-        try client.pack.write(.{ @intFromEnum(MessageType.Request), client.id, wrapStr(method), params });
-        client.id += 1;
-    }
+/// ResponseMessage for Job
+const ResponseMessage = struct {
+    msgid: u32,
+    /// this is a buffer which store err data
+    err: []const u8,
+    /// this is a buffer which store result data
+    result: []const u8,
 };
 
-/// this may be merged to tcpclient
-pub const UnixSocket = struct {
-    allocator: Allocator,
-    id: u32 = 0,
-    stream: std.net.Stream,
-    pack: streamPack,
-
-    /// init to create unix socket
-    /// this is only useful for unix
-    pub fn init(allocator: Allocator, path: []const u8) !UnixSocket {
-        const stream = try std.net.connectUnixSocket(path);
-
-        return UnixSocket{
-            .allocator = allocator,
-            .stream = stream,
-            .pack = streamPack.init(stream, stream),
-        };
-    }
-
-    pub fn deinit(client: *UnixSocket) void {
-        client.stream.close();
-    }
-
-    pub fn call(client: *UnixSocket, method: []const u8, params: anytype) !void {
-        try client.pack.write(.{ @intFromEnum(MessageType.Request), client.id, wrapStr(method), params });
-        client.id += 1;
-    }
+/// NotificationMessage for Job
+const NotificationMessage = struct {
+    method: []const u8,
+    params: []const u8,
 };
+
+/// Job for the JobQueue
+const Job = union(enum) {
+    request: RequestMessage,
+    response: ResponseMessage,
+    notification: NotificationMessage,
+};
+
+/// JobQueue
+const JobQueue = std.fifo.LinearFifo(Job, .Dynamic);
+
+/// Peer to Peer
+pub fn Peer(streamT: type) type {
+
+    // TODO: whether add context ?
+
+    // detect the declarations
+    comptime {
+        detectDecl(streamT, "WriteError");
+        detectDecl(streamT, "ReadError");
+        detectDecl(streamT, "write");
+        detectDecl(streamT, "read");
+    }
+
+    // build the steamT
+    const streamPackT = msgpack.Pack(
+        streamT,
+        streamT,
+        streamT.WriteError,
+        streamT.ReadError,
+        streamT.write,
+        streamT.read,
+    );
+
+    return struct {
+        const Self = @This();
+
+        allocator: Allocator,
+        id: u32 = 0,
+        stream: streamT,
+        pack: streamPackT,
+        job_queue: JobQueue,
+
+        /// init
+        pub fn init(allocator: Allocator, stream: streamT) Self {
+            return Self{
+                .allocator = allocator,
+                .stream = stream,
+                .pack = streamPack.init(stream, stream),
+                .job_queue = JobQueue.init(allocator),
+            };
+        }
+
+        /// deinit for Peer
+        /// NOTE: you need to deinit stream yourself
+        pub fn deinit(self: Self) void {
+            self.job_queue.deinit();
+        }
+
+        // TODO:
+        /// register method
+        pub fn register(name: []const u8, callback: anytype) !void {
+            _ = name;
+            _ = callback;
+        }
+
+        // TODO:
+        /// unregister method
+        pub fn unregister(name: []const u8) !void {
+            _ = name;
+        }
+
+        // TODO:
+        /// this will call method
+        fn call_method() !void {}
+
+        // TODO:
+        /// event loop
+        pub fn loop(self: Self) !void {
+            _ = self;
+        }
+
+        /// request
+        /// https://github.com/msgpack-rpc/msgpack-rpc/blob/master/spec.md#response-message
+        pub fn call(self: *Self, method: []const u8, params: anytype) !u32 {
+            try self.pack.write(.{ @intFromEnum(MessageType.Request), self.id, wrapStr(method), params });
+            const res: u32 = self.id;
+            self.id += 1;
+            return res;
+        }
+
+        /// Notification
+        /// https://github.com/msgpack-rpc/msgpack-rpc/blob/master/spec.md#notification-message
+        pub fn notify(self: Self, method: []const u8, params: anytype) !void {
+            try self.pack.write(.{ @intFromEnum(MessageType.Notification), wrapStr(method), params });
+        }
+
+        /// Response
+        /// https://github.com/msgpack-rpc/msgpack-rpc/blob/master/spec.md#response-message
+        pub fn response(self: Self, id: u32, err: anytype, result: anytype) !void {
+            try self.pack.write(.{ @intFromEnum(MessageType.Response), id, err, result });
+        }
+
+        /// get response
+        pub fn getResponse(self: Self, errorType: type, resultType: type) !resultType {
+            if (@typeInfo(errorType) != .Optional) {
+                const err_msg = comptimePrint("errorType ({}) must be optional type!", .{errorType});
+                @compileError(err_msg);
+            }
+            const resType = makeResTupleT(errorType, resultType);
+
+            const res = try self.pack.read(resType, self.allocator);
+            return res[3];
+        }
+    };
+}
+
+/// Checks whether a type contains a specified declaration
+fn detectDecl(T: type, comptime name: []const u8) void {
+    if (!@hasDecl(T, name)) {
+        @compileError(comptimePrint("sorry, type T  ({}) must have decl called {s}", .{ T, name }));
+    }
+}
+
+/// make res tuple type
+fn makeResTupleT(errorType: type, resType: type) type {
+    return @Type(std.builtin.Type.Struct{
+        .layout = .Auto,
+        .fields = &.{
+            .{
+                .alignment = @alignOf(u8),
+                .name = "0",
+                .type = u8,
+                .is_comptime = false,
+                .default_value = null,
+            },
+            .{
+                .alignment = @alignOf(u32),
+                .name = "1",
+                .type = u32,
+                .is_comptime = false,
+                .default_value = null,
+            },
+            .{
+                .alignment = @alignOf(errorType),
+                .name = "2",
+                .type = errorType,
+                .is_comptime = false,
+                .default_value = null,
+            },
+            .{
+                .alignment = @alignOf(resType),
+                .name = "3",
+                .type = resType,
+                .is_comptime = false,
+                .default_value = null,
+            },
+        },
+        .decls = &.{},
+        .is_tuple = true,
+    });
+}
--- src/serve.zig
+++ src/serve.zig
@@ -9,14 +9,16 @@ pub fn main() !void {
         const deinit_status = gpa.deinit();
         if (deinit_status == .leak) @panic("TEST FAIL");
     }
-    var client = try msgpack_rpc.TcpClient.init(allocator, "127.0.0.1", 9090);
+    const address = try std.net.Address.parseIp4("127.0.0.1", 9090);
+    const stream = try std.net.tcpConnectToAddress(address);
+    defer stream.close();
 
+    var client = msgpack_rpc.TcpClient.init(allocator, stream);
     defer client.deinit();
 
-    try client.call("nvim_get_api_info", .{});
+    _ = try client.call("nvim_get_current_tabpage", .{});
 
     var arr: [1000]u8 = undefined;
-
 
     const len = try client.stream.read(&arr);
     std.debug.print("len is {any}", .{arr[0..len]});
